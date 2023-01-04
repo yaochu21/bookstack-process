45b7bf1cd55650490b4cde2ac8cf6903d7884f73
fireindark707 <30530581+fireindark707@users.noreply.github.com>
2023-01-04 19:58:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from lib import extract
+from fastapi.middleware.cors import CORSMiddleware
 
 class Input(BaseModel):
     url: str
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
