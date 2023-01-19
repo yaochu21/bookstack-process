7fa96e9157af2d5084069f3855240ae0a53b06bc
yaochu21 <yaochu1828@gmail.com>
2023-01-19 10:33:43 -0600

    complete test page

--- lib/__init__.py
+++ lib/__init__.py
@@ -42,9 +42,27 @@ def extract(input):
     return pipe_data
 
 def generate(data):
-    # request = BookstackRequest(data)
-    # format subtitles
-    print(data)
+    request = BookstackRequest(data)
+    try:
+        request.convert_segments()
+    except Exception as e:
+        raise ValueError("Failed to convert segments: " + str(e))
+    try:
+        request.process_images()
+    except Exception as e:
+        raise ValueError("Failed to process images: " + str(e))
+    try:
+        request.stich_segments()
+    except Exception as e:
+        raise ValueError("Failed to stich segments: " + str(e))
+    try:
+        request.post()
+    except Exception as e:
+        raise ValueError("Failed to post: " + str(e))
+    try:
+        request.post_attachments()
+    except Exception as e:
+        raise ValueError("Failed to post attachments: " + str(e))
     return "None"
 
 if __name__ == "__main__":
@@ -66,8 +84,14 @@ if __name__ == "__main__":
     data = {}
     with open("../test/result.json",'r') as f:
         data = json.load(f)
-
-    print(data)
     
-    req = BookstackRequest(data)
+    req = BookstackRequest(json.dumps(data))
+    req.convert_segments()
+    req.process_images()
+    req.stich_segments()
+    req.post()
 
+    print(req.post_return)
+
+    with open('../test/body.txt','w') as f:
+        f.write(req.final_body)
--- lib/bookstack_request.py
+++ lib/bookstack_request.py
@@ -1,13 +1,13 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-# from session import Segment,SegmentType,Image,Subtitle
-from .session import Segment,SegmentType,Image,Subtitle
+from session import Segment,SegmentType,Image,Subtitle
+# from .session import Segment,SegmentType,Image,Subtitle
 
 # incomplete & deprecated, this request should be handled by the client side
 class BookstackRequest:
-    def __init__(self,data_dict):
-        # data_dict = json.loads(data)
+    def __init__(self,data):
+        data_dict = json.loads(data)
         self.html = data_dict['text']
         self.title = data_dict['title']
         self.year = data_dict['date']
@@ -19,50 +19,74 @@ class BookstackRequest:
         self.imgs = data_dict['imgs']
         self.segments = data_dict['segments']
 
-        self.api_url = "https://report.laodongqushi.com/api/pages"
         self.posted_id = None
+        self.post_return = None
 
         self.final_body = ""
+
+    def img_level_map(self,level):
+        if (level in [1,2,3]):
+            return 'h' + str(level+1)
+        if (level >= 4):
+            return 'h5'
+
+    def convert_segments(self):
+        segments = []
+        for seg_dict in self.segments:
+            seg_type = SegmentType(seg_dict['type'])
+
+            if (seg_type in [SegmentType.BODY,SegmentType.TABLE,SegmentType.NONE]):
+                seg = Segment(seg_dict['string'],seg_dict['s'],seg_dict['e'],seg_dict['tag'],SegmentType.BODY,seg_dict['order']) 
+                segments.append(seg)
+            elif (seg_type == SegmentType.SUBTITLE):
+                seg = Segment(seg_dict['string'],seg_dict['s'],seg_dict['e'],seg_dict['tag'],SegmentType.BODY,seg_dict['order']) 
+                seg = Subtitle(seg_dict['text'],seg_dict['level'],seg)
+                seg.tag = self.img_level_map(seg.level)
+                seg.string = "<%s>%s</%s>" % (seg.tag,seg.text,seg.tag)
+                segments.append(seg)
+        self.segments = segments
 
     def process_images(self):
         for img in self.imgs:
             if (not img['valid']):
                 continue
-            imgString = "<img src=" + img['src'] + " />"
-
-        # map images to segments
-        # add to list of segments
-        pass
-
-    def format_subtitles(self):
-        # for each subtitle segment, edit tag/html string according to level
-        pass
+            img_string = "<img src=\"%s\" referrerPolicy=\"no-referrer\" />" % img['url']
+            seg = Segment(img_string,-1,-1,"img",SegmentType.IMAGE,img['order'])
+            self.segments.append(seg)
 
     def stich_segments(self):
-        # join segments into string
-        # make html string with bs4
-        pass
+        self.segments = sorted(self.segments,key=lambda seg: seg.order)
+        seg_strings = [segment.string for segment in self.segments]
+        body = '\n'.join(seg_strings)
+        body = '<body>%s</body>' % body
 
-    def post_body(self):
+        bs = BeautifulSoup(body,"html.parser")
+        self.final_body = bs.prettify()
+
+    def post(self):
         tags = [{"name":"发布年份","value":self.year},
                 {"name":"地区","value":self.area}]
-        tags.extend([{"name":"作者/机构","value":entry} for entry in self.data_dict["author"]])
-        tags.extend([{"name":"调查方式","value":entry} for entry in self.data_dict["rtype"]])
-        tags.extend([{"name":"关键词","value":entry} for entry in self.data_dict["tags"]])
-        book_id = 0
+        tags.extend([{"name":"作者/机构","value":entry} for entry in self.authors])
+        tags.extend([{"name":"调查方式","value":entry} for entry in self.rtype])
+        tags.extend([{"name":"关键词","value":entry} for entry in self.keywords])
+        book_id = 1
 
         post_data = {
             "book_id":book_id,
-            "name":self.title,
-            "html":self.html,
+            "name":self.title + "-测试页面",
+            "html":self.final_body,
             "tags":tags
         }
         headers = {
             "Authorization":"Token vZdwWsto1ZJHjyvQ9mDmzp9dZTDh3Z7I:OxrfZbHz09xnUSbm6mRb7aY1XtwjRmU8",
         }
 
-        ret = requests.post(self.api_url,json=post_data,headers=headers)
+        ret = requests.post("https://report.laodongqushi.com/api/pages",json=post_data,headers=headers)
+        if (not ret.ok):
+            raise Exception("Bookstack api invalid response: %s" % ret.status_code)
+        
         ret.encoding = 'utf-8'
+        self.post_return = ret.text
 
     def post_attachments():
         pass
